514ce95f0d98fe9c5873d737283aaa7492228e2c
Ragnar Helgi <ragnarhelgi22@gmail.com>
2019-06-26 21:55:58 +0000

    v1.1
    added list of available colors

--- colors.py
+++ colors.py
@@ -3,3 +3,6 @@ from colorama import Fore
 color = [Fore.BLUE, # bots main color [0]
          Fore.GREEN, # sucess [1]
          Fore.RED] # Fail [2]
+
+# Fore: BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE, RESET.
+# https://pypi.org/project/colorama/
